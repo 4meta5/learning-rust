3a32edcd152ab33ea0e54282b470efe3c1f8d953
Amar Singh <asinghchrony@protonmail.com>
2018-12-03 00:59:21 -0500

    playing with hashing crates

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,15 +1,13 @@
 /// this will be interesting
 
+#[cfg(feature = "std")]
 extern crate blake2_rfc;
+#[cfg(feature = "std")]
+extern crate twox_hash;
+
+#[cfg(feature = "std")]
 extern crate ring;
+#[cfg(feature = "std")]
 extern crate untrusted;
 
 pub mod ed25519;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
