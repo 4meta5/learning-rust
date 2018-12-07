80cb6eeb737796b387d7bc2109665ca081a60147
Amar Singh <asinghchrony@protonmail.com>
2018-12-06 23:25:48 -0500

    first iteration for sss is sort of done

--- crypto/sss/src/lib.rs
+++ crypto/sss/src/lib.rs
@@ -3,7 +3,7 @@ extern crate error_chain;
 
 extern crate base64;
 extern crate merkle_sigs;
-extern crate protobuf; // I don't want to use protobufs; but this could be a good way to learn about them?
+extern crate protobuf;
 extern crate rand;
 extern crate ring;
 
